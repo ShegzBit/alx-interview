4f4c574901ce74e1a28360a76d8bd95ed86c4512
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-08 22:20:45 +0100

    Solved pascal task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,11 +6,29 @@ A module for pascal triangle implementation in python
 
 def pascal_triangle(n):
     """
-    A pascal triangle implementation in python
+    Pascal triangle
     """
+    default = [[1], [1, 1]]
     if n <= 0:
         return []
-    if n == 1:
-        return [1]
-    if n == 2:
-        return [1, 1]
+    if n - 1 < len(default):
+        return default[:n]
+    triangle = default[:]
+    # looping through the outer list
+    for i in range(1, n - 1):
+        j = 0
+        new_row = []
+        new_row.append(1)
+        # looping through each inner list to use value
+        # for generation of new list
+        while j < len(triangle[i]) - 1:
+            prev = triangle[i][j]
+            if j + 1 < len(triangle[i]):
+                # adding current value to next row
+                cur = prev + triangle[i][j + 1]
+            new_row.append(cur)
+            j += 1
+        new_row.append(1)
+        # adding new pascal row to the main triangle
+        triangle.append(new_row)
+    return triangle
